538d54b1b34a8bc89a8801e8589aae7d1f1714ca
Aleksandr <panchenkoalex@hotmail.com>
2021-02-15 22:10:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// const YearsOld = '17';
+// console.log(YearsOld);
+
+// const answer = prompt("Вам есть 18?", YearsOld);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Введите имя ', '');
+// answers[1] = prompt('Введите фамилию ', '');
+// answers[2] = prompt('Сколько вам лет? ', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+// const AnswersYears = answers[2];
+// console.log(AnswersYears);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// console.log('https://someurl.com/' + category + '/' + '5');
+
+// const user = 'Ivan';
+
+// alert(`Privet,${user}`);
+
+// console.log('arr' + " - object");
+
+// let incr = 10,
+//     decro = 10;
+
+// console.log (++incr);
+// console.log(--decro);
+
+// console.log(2*4 === 8);
+
+// console.log(2 + 2 * 2 === 8);
+
+// const  isChecked = false,
+//         isClose = false;
+
+// console.log(isChecked || !isClose);
