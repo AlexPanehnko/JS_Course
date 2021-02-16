30686ddb5ba81328824c20871765a23162d4b699
Aleksandr <panchenkoalex@hotmail.com>
2021-02-16 11:25:53 +0300

    First working script

--- js/JS_Practic_1/script.js
+++ js/JS_Practic_1/script.js
@@ -15,3 +15,13 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из просмотренных фильмов', ''),
+        b = prompt ('На сколько оцените его?', ''),
+        c = prompt('Один из просмотренных фильмов', ''),
+        d = prompt ('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
